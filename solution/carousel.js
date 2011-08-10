c1155d27973b5bdca757f6e85e4f3f15fde795ab
joaquin benitez <Toily@.(none)>
2011-08-10 03:28:54 -0500

    scroll initial gallery functionality complete, tooltip also working already, next: click to cast full gallery view :D

--- solution/carousel.js
+++ solution/carousel.js
@@ -3,26 +3,68 @@
 	Coder: Joaquin Benitez, Mexico City
 */
 
+/*********************************** CONFIGURATION */
+//(100px thumbnail width + margin)
+var thumb_width = 90;
+
+
 //wait for dom to complete
 $(document).ready(function(){
 
 	//hardcoded data
-	var POST_DATA = [{name: 'NES Images',album_image: 'images/gallery/nes.jpg',images: ['path_to_image/nes-01.jpg','path_to_image/nes-02.jpg','path_to_image/nes-03.jpg','path_to_image/nes-04.jpg','path_to_image/nes-05.jpg','path_to_image/nes-06.jpg','path_to_image/nes-07.jpg','path_to_image/nes-08.jpg','path_to_image/nes-09.jpg','path_to_image/nes-10.jpg','path_to_image/nes-11.jpg','path_to_image/nes-12.jpg','path_to_image/nes-13.jpg','path_to_image/nes-14.jpg','path_to_image/nes-15.png','path_to_image/nes-16.jpg','path_to_image/nes-17.jpg','path_to_image/nes-18.jpg','path_to_image/nes-19.gif']},{name: 'SNES Images',album_image: 'images/gallery/snes.jpg',images: ['path_to_image/snes-01.jpg','path_to_image/snes-02.jpg','path_to_image/snes-03.jpg','path_to_image/snes-04.jpg','path_to_image/snes-05.jpg','path_to_image/snes-06.jpg','path_to_image/snes-07.jpg','path_to_image/snes-08.jpg','path_to_image/snes-09.jpg','path_to_image/snes-10.jpg','path_to_image/snes-11.JPG','path_to_image/snes-12.jpg','path_to_image/snes-13.gif','path_to_image/snes-14.png','path_to_image/snes-15.jpg']},{name: 'N64 Images',album_image: 'images/gallery/n64.jpg',images: ['path_to_image/n64-01.jpg','path_to_image/n64-02.jpg','path_to_image/n64-03.jpg','path_to_image/n64-04.jpg','path_to_image/n64-05.jpg','path_to_image/n64-06.jpg','path_to_image/n64-07.jpg','path_to_image/n64-08.jpg','path_to_image/n64-09.jpg','path_to_image/n64-10.jpg','path_to_image/n64-11.jpg','path_to_image/n64-12.jpg','path_to_image/n64-13.jpg','path_to_image/n64-14.jpg']},{name: 'Game Cube Images',album_image: 'images/gallery/gamecube.jpg',images: ['path_to_image/gc-01.jpg','path_to_image/gc-02.jpg','path_to_image/gc-03.jpg','path_to_image/gc-04.jpg','path_to_image/gc-05.jpg','path_to_image/gc-06.jpg','path_to_image/gc-07.jpg','path_to_image/gc-08.jpg','path_to_image/gc-09.jpg','path_to_image/gc-10.jpg','path_to_image/gc-11.jpg','path_to_image/gc-12.jpg','path_to_image/gc-13.jpg','path_to_image/gc-14.jpg','path_to_image/gc-15.jpg']},{name: 'Wii Images',album_image: 'images/gallery/wii.jpg',images: ['path_to_image/wii-01.jpg','path_to_image/wii-02.jpg','path_to_image/wii-03.jpg','path_to_image/wii-04.jpg','path_to_image/wii-05.jpg','path_to_image/wii-06.jpg','path_to_image/wii-07.jpg','path_to_image/wii-08.jpg','path_to_image/wii-09.jpg']},{name: 'Wii-U Images',album_image: 'images/gallery/wiiu.jpg',images: ['path_to_image/wiiu-01.jpg','path_to_image/wiiu-02.jpg','path_to_image/wiiu-03.jpg','path_to_image/wiiu-04.jpg','path_to_image/wiiu-05.jpg']}];
+	var POST_DATA = [{name: 'NES Images',album_image: 'images/gallery/nes.jpg',images: ['images/gallery/nes-01.jpg','images/gallery/nes-02.jpg','images/gallery/nes-03.jpg','images/gallery/nes-04.jpg','images/gallery/nes-05.jpg','images/gallery/nes-06.jpg','images/gallery/nes-07.jpg','images/gallery/nes-08.jpg','images/gallery/nes-09.jpg','images/gallery/nes-10.jpg','images/gallery/nes-11.jpg','images/gallery/nes-12.jpg','images/gallery/nes-13.jpg','images/gallery/nes-14.jpg','images/gallery/nes-15.png','images/gallery/nes-16.jpg','images/gallery/nes-17.jpg','images/gallery/nes-18.jpg','images/gallery/nes-19.gif']},{name: 'SNES Images',album_image: 'images/gallery/snes.jpg',images: ['images/gallery/snes-01.jpg','images/gallery/snes-02.jpg','images/gallery/snes-03.jpg','images/gallery/snes-04.jpg','images/gallery/snes-05.jpg','images/gallery/snes-06.jpg','images/gallery/snes-07.jpg','images/gallery/snes-08.jpg','images/gallery/snes-09.jpg','images/gallery/snes-10.jpg','images/gallery/snes-11.JPG','images/gallery/snes-12.jpg','images/gallery/snes-13.gif','images/gallery/snes-14.png','images/gallery/snes-15.jpg']},{name: 'N64 Images',album_image: 'images/gallery/n64.jpg',images: ['images/gallery/n64-01.jpg','images/gallery/n64-02.jpg','images/gallery/n64-03.jpg','images/gallery/n64-04.jpg','images/gallery/n64-05.jpg','images/gallery/n64-06.jpg','images/gallery/n64-07.jpg','images/gallery/n64-08.jpg','images/gallery/n64-09.jpg','images/gallery/n64-10.jpg','images/gallery/n64-11.jpg','images/gallery/n64-12.jpg','images/gallery/n64-13.jpg','images/gallery/n64-14.jpg']},{name: 'Game Cube Images',album_image: 'images/gallery/gamecube.jpg',images: ['images/gallery/gc-01.jpg','images/gallery/gc-02.jpg','images/gallery/gc-03.jpg','images/gallery/gc-04.jpg','images/gallery/gc-05.jpg','images/gallery/gc-06.jpg','images/gallery/gc-07.jpg','images/gallery/gc-08.jpg','images/gallery/gc-09.jpg','images/gallery/gc-10.jpg','images/gallery/gc-11.jpg','images/gallery/gc-12.jpg','images/gallery/gc-13.jpg','images/gallery/gc-14.jpg','images/gallery/gc-15.jpg']},{name: 'Wii Images',album_image: 'images/gallery/wii.jpg',images: ['images/gallery/wii-01.jpg','images/gallery/wii-02.jpg','images/gallery/wii-03.jpg','images/gallery/wii-04.jpg','images/gallery/wii-05.jpg','images/gallery/wii-06.jpg','images/gallery/wii-07.jpg','images/gallery/wii-08.jpg','images/gallery/wii-09.jpg']},{name: 'Wii-U Images',album_image: 'images/gallery/wiiu.jpg',images: ['images/gallery/wiiu-01.jpg','images/gallery/wiiu-02.jpg','images/gallery/wiiu-03.jpg','images/gallery/wiiu-04.jpg','images/gallery/wiiu-05.jpg']}];
 	
 	//parse array using jQuery
+	var built_gallery = '';
 	$.each(POST_DATA, function(index,value){
-		console.log(value.name);
+		//generate and append markup
+		built_gallery+='<div class="gal-'+index+' thumb left track-as-link" tool-tip="'+value.name+'"><img src="'+value.album_image+'"></div>'
 	});
 
-	//create "fancy" (not that fancy actually :P)
+	//adjust gallery css properties so it renders correctly
+	var gallery_width = ($(built_gallery).length*thumb_width) + 12; //12 last margin + 2px border correction
+	$('.thumb-strip').width( gallery_width );
 
+	//append gallery to strip gallery
+	$('.thumb-strip').append(built_gallery);
 
+	//scroll functionality
+	//get far left scroll value
+	var left_scroll_value = $('.gallery-menu').width() - gallery_width;
 
-	$('[tool-tip]').hover(
+	$('.gallery-scroll-left').hover(
 		function(){
-			$('#tool-tip-holder').fadeIn();
+			var my_gallery = $(this).parent().find('.thumb-strip');
+			$(my_gallery).animate({marginLeft:left_scroll_value},{queue:false,duration:2000,easing:'linear'});
+		}
+		,
+		function(){
+			var my_gallery = $(this).parent().find('.thumb-strip');
+			$(my_gallery).stop();
+		}
+	);
+
+	$('.gallery-scroll-right').hover(
+		function(){
+			var my_gallery = $(this).parent().find('.thumb-strip');
+			$(my_gallery).animate({marginLeft:10},{queue:false,duration:2000,easing:'linear'});
+		}
+		,
+		function(){
+			var my_gallery = $(this).parent().find('.thumb-strip');
+			$(my_gallery).stop();
+		}
+	);
+
+	/* NOT THAT FANCY TOOLTIP FUNTIONALITY*/
+	$('[tool-tip]').hover(
+		//over
+		function(){
+			//$('#tool-tip-holder').fadeIn(); //some display persistance issues :'(
+			$('#tool-tip-holder').show(); //hides and shows correctly
+
 			$('#tool-tip-holder').text($(this).attr('tool-tip'));
-			$(document).bind('mousemove', function(e){
+			$(this).bind('mousemove', function(e){
 				$('#tool-tip-holder').css({
 					left:  e.pageX,
 					top:   e.pageY+20
@@ -30,10 +72,10 @@ $(document).ready(function(){
 			});
 		}
 		,
+		//leave
 		function(){
 			$('#tool-tip-holder').hide();
-			$(document).unbind('mousemove');
-			console.log('off');
+			$(this).unbind('mousemove');
 		});
 
 });
